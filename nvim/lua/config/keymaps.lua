bc1561bc5c39a8bda95550d9990b8a87f8213540
taiyi <taiyijiang@anuegroup.com.tw>
2024-11-08 16:27:23 +0800

    bak

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+-- Insert
+keymap("i", "jj", "<ESC>", opts)
+
+-- Compiler code
+keymap("n", ",c", ":r!python3 %<CR>", opts)
